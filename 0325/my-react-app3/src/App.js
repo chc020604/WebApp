be6d93ffcf222627f676a2c22e8409dc063d1ac0
chc020604 <chc020604@gmail.com>
2026-03-25 11:43:40 +0900

    4주차

---
+++ 0325/my-react-app3/src/App.js
@@ -0,0 +1,13 @@
+import "./App.js"
+import Clock from "./Clock";
+// Clock 컴포넌트 불러오기
+function App() {
+  return (
+    <div>
+      <Clock />
+      <Clock />
+      <Clock />
+    </div>
+  );
+}
+export default App;
